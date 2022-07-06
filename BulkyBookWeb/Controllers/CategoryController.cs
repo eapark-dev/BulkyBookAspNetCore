97e9a2dac2e14122824b3340361396177868d598
eunapark <eapark5908@gmail.com>
2022-07-06 16:41:50 +0900

    udemy - asp.net core 44강 진행중 (CategoriesController 추가)

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -18,5 +18,101 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //XSRf/CSRF 공격 방지용
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "이름과 주문번호는 같을 수 없습니다.");
+            }
+
+            //정확한 데이터가 들어왔는 지 체크 
+            if (ModelState.IsValid)
+            { 
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "등록이 완료되었습니다.";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //EDIT
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+                return NotFound();
+
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.FirstOrDefault(u => u.Id == id);
+
+            if(categoryFromDb == null)
+                return NotFound();
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //XSRf/CSRF 공격 방지용
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "이름과 주문번호는 같을 수 없습니다.");
+            }
+
+            //정확한 데이터가 들어왔는 지 체크 
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        //Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.FirstOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+                return NotFound();
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken] //XSRf/CSRF 공격 방지용
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+                return NotFound();
+
+            //정확한 데이터가 들어왔는 지 체크 
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "삭제가 완료되었습니다.";
+            return RedirectToAction("Index");
+        }
     }
 }
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BulkyBookWeb.Models
 {
+    [Table("Categories")]
     public class Category
     {
         [Key]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "주문 번호는 1에서 100까지의 숫자 안에서 지정이 가능합니다.")] //범위 최소값과 최대값 지정 
         public int DisplayOrder { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
 
