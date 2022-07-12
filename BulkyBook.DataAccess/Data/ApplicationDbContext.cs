858c660707ff798944f2296d75ec3c73d39ea857
eunapark <eapark5908@gmail.com>
2022-07-12 10:47:36 +0900

    udemy - .net core 강의 102강 완료

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -16,4 +16,5 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<CoverType> CoverTypes { get; set; }
+    public DbSet<Product> Products { get; set; }
 }
